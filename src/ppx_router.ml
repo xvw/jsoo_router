a8da55404554eac075026a3b2808df9601db1b03
xvw <xavier.vdw@gmail.com>
2016-11-17 16:53:28 +0100

    Append Reg Module

--- src/ppx_router.ml
+++ src/ppx_router.ml
@@ -18,3 +18,51 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
 *)
+
+let raise_error ?(loc = !Ast_helper.default_loc) message =
+  let open Location in
+raise (Error (error ~loc message))
+
+
+(* Regexp utils *)
+module Reg =
+struct
+
+  (* Record all types in a Hashtbl *)
+  let _types = Hashtbl.create 6
+  let _ =
+    let add = Hashtbl.add in
+    add _types "int" "(-?[0-9]+)";
+    add _types "float" "(-?[0-9]+.[0-9]*)";
+    add _types "char" "(.)";
+    add _types "bool" "(true|false)"; 
+    add _types "string" "(.+)"
+
+  (* Create regex *)
+  let cons = Printf.sprintf "%s%c"
+  let create str =
+    let hash = Hashtbl.create 10 in 
+    let len  = String.length str in
+    let rec aux accn acc i =
+      if i = len then (acc, accn, hash)
+      else
+        match str.[i] with
+        | '{' ->
+          let (r, new_index) = variable "" (succ i) in
+          let index = succ accn in
+          let _ = Hashtbl.add hash index r in
+          aux index (acc ^ (Hashtbl.find _types r)) new_index
+        | c   -> aux accn (cons acc c) (succ i)
+    and variable acc i =
+      if i = len then raise_error "Malformed route"
+      else
+        match str.[i] with
+        | '}' ->
+          if Hashtbl.mem _types acc then (acc, succ i)
+          else raise_error "Unknown type in route"
+        | c -> variable (cons acc c) (succ i)
+                 
+    in aux 0 "" 0
+      
+
+end
